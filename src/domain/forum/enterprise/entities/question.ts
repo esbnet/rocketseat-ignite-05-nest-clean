68f40df620f93741123d2042fb30ed6f419bb5a2
esbnet <esbnet@gmail.com>
2023-09-29 14:11:37 -0300

    feat: :sparkles: created comunication between layers (mappers)

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -14,7 +14,7 @@ export interface IQuestionProps {
   slug: Slug
   attachments: QuestionAttachmentList
   createdAt: Date
-  updatedAt?: Date
+  updatedAt?: Date | null
 }
 
 export class Question extends AggregateRoot<IQuestionProps> {
