d3b72fd4e4738be3970980dd92244db8460652b2
esbnet <esbnet@gmail.com>
2023-09-29 13:51:17 -0300

    feat: :sparkles: create prisma repositories

---
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class PrismaQuestionsRepository implements QuestionsRepository {
+  findById(id: string): Promise<Question | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findBySlug(slug: string): Promise<Question | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findManyRecent(params: PaginationParams): Promise<Question[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  create(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  update(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
+import { PrismaService } from '../database/prisma/prisma.service'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { ListQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
