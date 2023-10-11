5cf95621b28387f087ea731b3e56a7aad212fb69
esbnet <esbnet@gmail.com>
2023-10-10 21:53:51 -0300

    feat: :sparkles: created delete question controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,6 @@
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
@@ -10,6 +11,7 @@ import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
+import { DeleteQuestionController } from './controllers/delete-question-controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { ListQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
@@ -20,12 +22,14 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    DeleteQuestionController,
     EditQuestionController,
     ListQuestionsController,
     GetQuestionBySlugController,
   ],
   providers: [
     CreateQuestionUseCase,
+    DeleteQuestionUseCase,
     EditQuestionUseCase,
     FetchRecentQuestionsUseCase,
     RegisterStudentUseCase,
