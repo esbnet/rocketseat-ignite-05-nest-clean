69cfcaafdb553200afd82464d45f14f2ba459896
esbnet <esbnet@gmail.com>
2023-10-11 21:09:38 -0300

    feat: :sparkles: created fetch question comments controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -11,6 +11,7 @@ import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cas
 import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
 import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
@@ -30,8 +31,9 @@ import { DeleteQuestionCommentController } from './controllers/delete-question-c
 import { DeleteQuestionController } from './controllers/delete-question.controller'
 import { EditAnswerController } from './controllers/edit-answer.controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
+import { FetchQuestionAnswersController } from './controllers/fetch-question-answer.controller'
+import { FetchQuestionCommentsController } from './controllers/fetch-question-comments.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { FetchQuestionAnswersController } from './controllers/fetche-question-answer.controller'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
 
 @Module({
@@ -53,6 +55,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     DeleteQuestionCommentController,
     DeleteAnswerCommentController,
     CommentOnAnswerController,
+    FetchQuestionCommentsController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -71,6 +74,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     DeleteQuestionCommentUseCase,
     DeleteAnswerCommentUseCase,
     CommentOnAnswerUseCase,
+    FetchQuestionCommentsUseCase,
   ],
 })
 export class HttpModule {}
